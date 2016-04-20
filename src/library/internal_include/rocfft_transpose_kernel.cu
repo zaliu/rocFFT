6de2cdbe016cc3893bb49716200cdc919ea1001d
tester <timmy.liu@amd.com>
2016-04-20 12:37:29 -0400

    added case to launch kernel with anysizes

--- src/library/internal_include/rocfft_transpose_kernel.cu
+++ src/library/internal_include/rocfft_transpose_kernel.cu
@@ -1,6 +1,6 @@
 #include "hip_runtime.h"
 
-template<typename T, int micro_tile_col_size, int micro_tile_row_size>
+template<typename T, int micro_tile_col_size, int micro_tile_row_size, int wg_col_size, int wg_row_size>
 inline __global__ void transpose_kernel_outplace(hipLaunchParm lp, T *input_matrix, T *output_matrix, size_t input_row_size, size_t input_col_size, size_t batch_size)
 {
     // WG size can be assumed to be 16 by 16
@@ -12,13 +12,14 @@ inline __global__ void transpose_kernel_outplace(hipLaunchParm lp, T *input_matr
     size_t block_dim_1 = hipBlockDim_y;// size of work groups 16
     size_t grid_dim_0 = hipGridDim_x;// number of blocks
 
-    // for 64 x 64 micro tile size we will need 16 x 4 x 64 blocks (4 x 64 == 16 x 16)
-    // for 32 x 32 micro tile size we will need  4 x 8 x 32 blocks
+    // for 64 x 64 macro tile size we will need 16 x 4 x 64 blocks (4 x 64 == 16 x 16)
+    // for 32 x 32 macro tile size we will need  4 x 8 x 32 blocks
+    const size_t macro_tile_col_size = micro_tile_col_size * wg_col_size;
+    const size_t macro_tile_row_size = micro_tile_row_size * wg_row_size;
+    const size_t reshape_factor = macro_tile_col_size / block_dim_0; // 64 / 16 = 4 need to fit 4 rows into one row in LDS; 32 / 16 = 2
+    const size_t unroll_factor = macro_tile_row_size / (block_dim_1 / reshape_factor); // 64 / (16 / 4) = 16; 32 / (16 / 2) = 4
 
-    const size_t reshape_factor = micro_tile_col_size / block_dim_0; // 64 / 16 = 4 need to fit 4 rows into one row in LDS; 32 / 16 = 2
-    const size_t unroll_factor = micro_tile_row_size / (block_dim_1 / reshape_factor); // 64 / (16 / 4) = 16; 32 / (16 / 2) = 4
-
-    __shared__ T lds[micro_tile_row_size][micro_tile_col_size];
+    __shared__ T lds[macro_tile_row_size][macro_tile_col_size];
 
     size_t batch_idx = 0;
     size_t blocks_per_batch = grid_dim_0 / batch_size;
@@ -27,8 +28,8 @@ inline __global__ void transpose_kernel_outplace(hipLaunchParm lp, T *input_matr
     input_matrix += batch_idx * input_col_size * input_row_size;
 
     size_t input_offset = 0;
-    input_offset += input_col_size * block_idx_1 * micro_tile_row_size;// each WG works on 64 by 64 block or 32 by 32
-    input_offset += (block_idx_0 % blocks_per_batch) * micro_tile_col_size;
+    input_offset += input_col_size * block_idx_1 * macro_tile_row_size;// each WG works on 64 by 64 block or 32 by 32
+    input_offset += (block_idx_0 % blocks_per_batch) * macro_tile_col_size;
 
     input_matrix += input_offset;
     for(int i = 0; i < unroll_factor; i++)
@@ -45,8 +46,8 @@ inline __global__ void transpose_kernel_outplace(hipLaunchParm lp, T *input_matr
 
     output_matrix += batch_idx * input_col_size * input_row_size;
     size_t output_offset = 0;
-    output_offset += input_row_size * (block_idx_0 % blocks_per_batch) * micro_tile_row_size;//input_row_size == ouput_col_size
-    output_offset += block_idx_1 * micro_tile_col_size;
+    output_offset += input_row_size * (block_idx_0 % blocks_per_batch) * macro_tile_row_size;//input_row_size == ouput_col_size
+    output_offset += block_idx_1 * macro_tile_col_size;
 
     output_matrix += output_offset;
 
--- src/library/rocfft_transpose.cpp
+++ src/library/rocfft_transpose.cpp
@@ -48,17 +48,31 @@ rocfft_transpose_status rocfft_transpose_plan_create( rocfft_transpose_plan *pla
 
 rocfft_transpose_status rocfft_transpose_outplace_real(const rocfft_transpose_plan plan, void *in_buffer, void *out_buffer)
 {
-    int block_size = 16;//dim3(input_row_size/16/4, input_col_size/16/4, 1)
-    const int micro_tile_size = 64;
+    const int block_size_16 = 16;//dim3(input_row_size/16/4, input_col_size/16/4, 1)
+    const int block_size_8 = 8;
+    const int block_size_1 = 1;
+    const int micro_tile_size_1 = 1;
+    const int micro_tile_size_4 = 4;
     int input_row_size = plan->lengths[1];
     int input_col_size = plan->lengths[0];
     int batch_size = plan->batch;
-    //this is hip bug that I have to reverse the dim3 values
-    hipLaunchKernel(HIP_KERNEL_NAME(transpose_kernel_outplace<float,micro_tile_size,micro_tile_size>), dim3(input_col_size/micro_tile_size * batch_size, input_row_size/micro_tile_size), dim3(block_size, block_size, 1), 0, 0, (float*)in_buffer, (float*)out_buffer, input_row_size, input_col_size, batch_size );
+    
+    if(input_row_size % (block_size_16*micro_tile_size_4) == 0 && input_col_size % (block_size_16*micro_tile_size_4) == 0)
+    {
+        hipLaunchKernel(HIP_KERNEL_NAME(transpose_kernel_outplace<float,micro_tile_size_4,micro_tile_size_4,block_size_16,block_size_16>), dim3(input_col_size/micro_tile_size_4/block_size_16 * batch_size, input_row_size/micro_tile_size_4/block_size_16), dim3(block_size_16, block_size_16, 1), 0, 0, (float*)in_buffer, (float*)out_buffer, input_row_size, input_col_size, batch_size );
+    }
+    else if(input_row_size % (block_size_1*micro_tile_size_1) == 0 && input_col_size % (block_size_1*micro_tile_size_1) == 0)
+    {
+    // the kernel should be able to work on any size with no guarantee of performance 
+    hipLaunchKernel(HIP_KERNEL_NAME(transpose_kernel_outplace<float,micro_tile_size_1,micro_tile_size_1,block_size_1,block_size_1>), dim3(input_col_size/micro_tile_size_1/block_size_1 * batch_size, input_row_size/micro_tile_size_1/block_size_1), dim3(block_size_1, block_size_1, 1), 0, 0, (float*)in_buffer, (float*)out_buffer, input_row_size, input_col_size, batch_size );
+    }
+    else
+    {
+        return rocfft_transpose_status_not_implemented;
+    }    
+    
     hipDeviceSynchronize();
-
-
-   return rocfft_transpose_status_success;
+    return rocfft_transpose_status_success;
 }
 
 rocfft_transpose_status rocfft_transpose_execute( const rocfft_transpose_plan plan,
