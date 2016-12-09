043c875febd91f035bb696982560bcdbae6e16f0
bragadeesh <bragadeesh@users.noreply.github.com>
2016-12-09 14:48:00 -0800

    removing old transpose related code for now, can reinstate later as plans allow

--- library/src/device_function_include/rocfft_transpose_kernel_check_boundary.cu
+++
@@ -1,7 +0,0 @@
-
-
-template<typename T, int micro_tile_col_size, int micro_tile_row_size, int wg_col_size, int wg_row_size>
-__global__ void transpose_kernel_outplace_check_boundary(hipLaunchParm lp, T *input_matrix, T *output_matrix, size_t input_row_size, size_t input_col_size, size_t input_leading_dim_size, size_t output_leading_dim_size, size_t batch_size)
-{
-//TODO
-}
