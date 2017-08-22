fdc172bbffa1e55d758591910c746fba2806433c
tim <tingxingdong@gmail.com>
2017-08-22 16:57:30 -0500

    minor

--- library/src/device/kernels/transpose.h
+++ library/src/device/kernels/transpose.h
@@ -5,7 +5,7 @@
 #include <hip/hip_runtime.h>
 #include "butterfly.h"
 
-//works for real to real and complex interleaved to complex interleaved 
+//works for real to real and complex interleaved to complex interleaved
 template<typename T, int micro_tile_col_size, int micro_tile_row_size, int wg_col_size, int wg_row_size>
 __global__ void transpose_kernel_outplace(hipLaunchParm lp, T *input_matrix, T *output_matrix, T *twiddles_large,
         const size_t batch_count, const size_t dim, const size_t *lengths, const size_t *stride_in, const size_t *stride_out)
@@ -39,13 +39,13 @@ __global__ void transpose_kernel_outplace(hipLaunchParm lp, T *input_matrix, T *
 
     {
     size_t counter_mod = block_idx_0 / blocks_per_matrix;
-    
+
     for(int i = dim; i>2; i--){
         int currentLength = 1;
         for(int j=2; j<i; j++){
             currentLength *= lengths[j];
         }
-    
+
         iOffset += (counter_mod / currentLength)*stride_in[i];
         oOffset += (counter_mod / currentLength)*stride_out[i];
         counter_mod = counter_mod % currentLength;
@@ -102,9 +102,9 @@ __global__ void transpose_kernel_outplace(hipLaunchParm lp, T *input_matrix, T *
 
 /*
    transpose input of size m * n (up to DIM_X * DIM_X) to output of size n * m
-   input, output are in device memory 
+   input, output are in device memory
    shared memory of size DIM_X*DIM_Y is allocated internally as working space
-   
+
    Assume DIM_X by DIM_Y threads are reading & wrting a tile size DIM_X * DIM_X
    DIM_X is divisible by DIM_Y
 */
@@ -119,7 +119,7 @@ transpose_tile_device(const T* input, T* output, int m, int n, int input_lda, in
     int tid = hipThreadIdx_x + hipThreadIdx_y * hipBlockDim_x;
     int tx1 = tid % DIM_X;
     int ty1 = tid / DIM_X;
-    
+
     #pragma unroll
     for(int i=0; i<n; i+=DIM_Y)
     {
@@ -128,11 +128,11 @@ transpose_tile_device(const T* input, T* output, int m, int n, int input_lda, in
             shared_A[ty1+i][tx1] = input[tx1 + (ty1 + i) * input_lda];   // the transpose taking place here
         }
     }
-    __syncthreads();// ? 
+    __syncthreads();// ?
 
     for(int i=0; i<m; i+=DIM_Y)
     {
-        //reconfigure the threads 
+        //reconfigure the threads
         if( tx1 < n && (ty1 + i)< m)
         {
             output[tx1 + (i + ty1) * output_lda] = shared_A[tx1][ty1+i];
@@ -143,11 +143,11 @@ transpose_tile_device(const T* input, T* output, int m, int n, int input_lda, in
 
 /*
    transpose input of size m * n to output of size n * m
-   input, output are in device memory 
+   input, output are in device memory
 
-   2D grid and 2D thread block 
-   the grid is orgnized into m * n matrix 
-   
+   2D grid and 2D thread block
+   the grid is orgnized into m * n matrix
+
    Assume DIM_X by DIM_Y threads are transposing each tile DIM_X * DIM_X
 */
 
@@ -158,16 +158,15 @@ __global__ void
 transpose_kernel( hipLaunchParm lp, int m, int n, const T* input, T* output, int input_lda, int output_lda )
 {
 
-    input += hipBlockIdx_x * DIM_X + hipBlockIdx_y * DIM_X * input_lda;   
+    input += hipBlockIdx_x * DIM_X + hipBlockIdx_y * DIM_X * input_lda;
     output += hipBlockIdx_x * DIM_X * output_lda + hipBlockIdx_y * DIM_X;
-    
-    int mm = min(m - hipBlockIdx_x * DIM_X, DIM_X); // the corner case along m 
-    int nn = min(n - hipBlockIdx_y * DIM_X, DIM_X); // the corner case along n 
+
+    int mm = min(m - hipBlockIdx_x * DIM_X, DIM_X); // the corner case along m
+    int nn = min(n - hipBlockIdx_y * DIM_X, DIM_X); // the corner case along n
 
     transpose_tile_device<T, DIM_X, DIM_Y>(input, output, mm, nn, input_lda, output_lda );
 }
 
-#endif // TRANSPOSE_DEVICE_H
 
 
 
