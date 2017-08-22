f800f51ee4212dda3ea0dae10cd9a43841f7a54c
tim <tingxingdong@gmail.com>
2017-08-22 17:38:37 -0500

    fix the batch count bug; let the z-dim naturally scale the batch count

--- library/src/device/kernels/transpose.h
+++ library/src/device/kernels/transpose.h
@@ -95,3 +95,75 @@ __global__ void transpose_kernel_outplace(hipLaunchParm lp, T *input_matrix, T *
 
 }
 
+
+
+/*
+   transpose input of size m * n (up to DIM_X * DIM_X) to output of size n * m
+   input, output are in device memory
+   shared memory of size DIM_X*DIM_X is allocated internally as working space
+
+   Assume DIM_X by DIM_Y threads are reading & wrting a tile size DIM_X * DIM_X
+   DIM_X is divisible by DIM_Y
+*/
+
+template<typename T, int DIM_X, int DIM_Y>
+__device__ void
+transpose_tile_device(const T* input, T* output, int m, int n, int input_lda, int output_lda )
+{
+
+    __shared__ T shared_A[DIM_X][DIM_X+1];//avoid bank conflicts
+
+    int tid = hipThreadIdx_x + hipThreadIdx_y * hipBlockDim_x;
+    int tx1 = tid % DIM_X;
+    int ty1 = tid / DIM_X;
+
+    #pragma unroll
+    for(int i=0; i<n; i+=DIM_Y)
+    {
+        if( tx1 < m && (ty1 + i) < n)
+        {
+            shared_A[ty1+i][tx1] = input[tx1 + (ty1 + i) * input_lda];   // the transpose taking place here
+        }
+    }
+    __syncthreads();// ?
+
+    for(int i=0; i<m; i+=DIM_Y)
+    {
+        //reconfigure the threads
+        if( tx1 < n && (ty1 + i)< m)
+        {
+            output[tx1 + (i + ty1) * output_lda] = shared_A[tx1][ty1+i];
+        }
+    }
+
+}
+
+/*
+   transpose input of size m * n to output of size n * m
+   input, output are in device memory
+
+   2D grid and 2D thread block (DIM_X, DIM_Y)
+
+   Assume DIM_X by DIM_Y threads are transposing a tile DIM_X * DIM_X
+*/
+
+
+
+template<typename T, int DIM_X, int DIM_Y>
+__global__ void
+transpose_kernel( hipLaunchParm lp, int m, int n, const T* input, T* output, int input_lda, int output_lda )
+{
+
+    input += hipBlockIdx_x * DIM_X + hipBlockIdx_y * DIM_X * input_lda + (hipBlockIdx_z * input_lda * n);
+    output += hipBlockIdx_x * DIM_X * output_lda + hipBlockIdx_y * DIM_X + (hipBlockIdx_z * ouput_lda * m);
+
+    int mm = min(m - hipBlockIdx_x * DIM_X, DIM_X); // the corner case along m
+    int nn = min(n - hipBlockIdx_y * DIM_X, DIM_X); // the corner case along n
+
+    transpose_tile_device<T, DIM_X, DIM_Y>(input, output, mm, nn, input_lda, output_lda );
+}
+
+
+
+
+
---
+++ library/src/device/transpose.cpp
@@ -0,0 +1,77 @@
+/*******************************************************************************
+ * Copyright (C) 2016 Advanced Micro Devices, Inc. All rights reserved.
+ ******************************************************************************/
+
+#include <iostream>
+#include "rocfft.h"
+#include "rocfft_hip.h"
+#include "kernels/transpose.h"
+
+
+
+/* ============================================================================================ */
+
+/*! \brief FFT Transpose out-of-place API
+
+    \details
+    transpose matrix A of size (m by n) to matrix B (n by m)
+
+    @param[in]
+    m         size_t.
+    @param[in]
+    n         size_t.
+    @param[in]
+    A     pointer storing batch_count of A matrix on the GPU.
+    @param[in]
+    lda
+              size_t
+              specifies the leading dimension for the matrix A
+    @param[inout]
+    B    pointer storing batch_count of B matrix on the GPU.
+    @param[in]
+    ldb
+              size_t
+              specifies the leading dimension for the matrix B
+    @param[in]
+    batch_count
+              size_t
+              number of matrices processed
+    ********************************************************************/
+
+
+template<typename T, int TRANSPOSE_DIM_X, int TRANSPOSE_DIM_Y>
+rocfft_status
+rocfft_transpose_outofplace_template(size_t m, size_t n, const T* A, size_t lda, T* B, size_t ldb, size_t batch_count)
+{
+
+    if (lda < m )
+        return rocfft_status_invalid_dimensions;
+    else if (ldb < n )
+        return rocfft_status_invalid_dimensions;
+
+    if(m == 0 || n == 0 ) return rocfft_status_success;
+
+    dim3 grid((m-1)/TRANSPOSE_DIM_X + 1, ( (n-1)/TRANSPOSE_DIM_X + 1 ), batch_count);
+    dim3 threads(TRANSPOSE_DIM_X, TRANSPOSE_DIM_Y, 1);
+
+    hipStream_t rocfft_stream = 0;
+
+    hipLaunchKernel(HIP_KERNEL_NAME(transpose_kernel<T, TRANSPOSE_DIM_X, TRANSPOSE_DIM_Y>), dim3(grid), dim3(threads), 0, rocfft_stream, m, n, A, B, lda, ldb);
+
+    return rocfft_status_success;
+
+}
+
+/* ============================================================================================ */
+
+
+
+rocfft_status
+rocfft_transpose_complex_to_complex(rocfft_precision precision, size_t m, size_t n, const void* A, size_t lda, void* B, size_t ldb, size_t batch_count)
+{
+    if( precision == rocfft_precision_single)
+        return rocfft_transpose_outofplace_template<float2, 64, 16>(m, n, (const float2*)A, lda, (float2*)B, ldb, batch_count);
+    else
+        return rocfft_transpose_outofplace_template<double2, 32, 32>(m, n, (const double2*)A, lda, (double2*)B, ldb, batch_count);//double2 must use 32 otherwise exceed the shared memory (LDS) size
+}
+
