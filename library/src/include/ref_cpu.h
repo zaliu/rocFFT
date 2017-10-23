a8758054ae5d55ff64243092c21648c566b4b9ad
bragadeesh <bragadeesh@users.noreply.github.com>
2017-10-23 15:44:57 -0700

    logic to access/pack from arbitrary strided data

--- library/src/include/ref_cpu.h
+++ library/src/include/ref_cpu.h
@@ -111,25 +111,88 @@ class RefLibOp
         memset(ot, 0x40, totalSize);
     }
 
+    void CopyInputVector(const void *data_p)
+    {
+        DeviceCallIn *data = (DeviceCallIn *)data_p;
+
+        size_t in_size = data->node->iDist * data->node->batch;
+        size_t in_size_bytes = in_size * 2 * sizeof(float);
+        local_fftwf_complex *tmp_mem = (local_fftwf_complex *)malloc(in_size_bytes);
+        hipMemcpy(tmp_mem, data->bufIn[0], in_size_bytes, hipMemcpyDeviceToHost);
+
+        size_t offset_in = 0;
+        size_t offset_tmp = 0;
+        size_t offset_tmp_d = 0;
+		size_t pos = 0;
+
+		std::vector<size_t> current;
+		for(size_t i=0; i<data->node->length.size(); i++) current.push_back(0);
+
+        size_t b = 0;
+        while(b<data->node->batch)
+        {
+            offset_tmp += b*data->node->iDist;
+        	while(true)
+            {
+                offset_tmp = offset_tmp_d + current[0]*data->node->inStride[0];
+    
+    			in[offset_in][0] = tmp_mem[offset_tmp][0];
+    			in[offset_in][1] = tmp_mem[offset_tmp][1];
+    			
+                current[0]++;
+                offset_in++;
+        
+                while(current[pos] == data->node->length[pos])
+                {
+                    if(pos == (data->node->length.size() - 1))
+                    {
+    					goto nested_exit;
+                    }
+        
+                    current[pos] = 0;
+                    pos++;
+                    current[pos]++;
+       
+    				for(size_t i=1; i<current.size(); i++)
+    					offset_tmp_d += current[i]*data->node->inStride[i]; 
+        
+                }
+        
+                pos = 0;
+            }
+nested_exit:
+            b++;
+		}
+
+        if(tmp_mem)
+            free(tmp_mem);
+    }
+
     void Execute(const void *data_p)
     {
+        DeviceCallIn *data = (DeviceCallIn *)data_p;
+
+        if(data->node->scheme == CS_KERNEL_STOCKHAM)
+        {
         RefLibHandle &refHandle = RefLibHandle::GetRefLibHandle();
         ftype_fftwf_plan_many_dft local_fftwf_plan_many_dft = (ftype_fftwf_plan_many_dft)dlsym(refHandle.fftw3f_lib, "fftwf_plan_many_dft");
         ftype_fftwf_execute local_fftwf_execute = (ftype_fftwf_execute)dlsym(refHandle.fftw3f_lib, "fftwf_execute");
         ftype_fftwf_destroy_plan local_fftwf_destroy_plan = (ftype_fftwf_destroy_plan)dlsym(refHandle.fftw3f_lib, "fftwf_destroy_plan");
 
-        DeviceCallIn *data = (DeviceCallIn *)data_p;
 
         int n[1];
-        n[0] = data->node->length[0];     
-        void *p = local_fftwf_plan_many_dft(1, n, 1, in, NULL, 1, 0, ot, NULL, 1, 0, LOCAL_FFTW_FORWARD, LOCAL_FFTW_ESTIMATE);
+        n[0] = data->node->length[0];
+        int howmany = data->node->batch;
+        for(size_t i=1; i<data->node->length.size(); i++) howmany *= data->node->length[i];
 
-        size_t in_size = data->node->iDist * data->node->batch;
-        size_t in_size_bytes = in_size * 2 * sizeof(float);
-        hipMemcpy(in, data->bufIn[0], in_size_bytes, hipMemcpyDeviceToHost);
-
+        void *p = local_fftwf_plan_many_dft(1, n, howmany, in, NULL, 1, n[0], ot, NULL, 1, n[0], LOCAL_FFTW_FORWARD, LOCAL_FFTW_ESTIMATE);
+        CopyInputVector(data_p);
         local_fftwf_execute(p);
         local_fftwf_destroy_plan(p);
+        }
+        else if(data->node->scheme == CS_KERNEL_TRANSPOSE)
+        {
+        }
     }
 
 public:
