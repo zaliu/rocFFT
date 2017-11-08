fbb4b3fabb7b8766c51606abb3f2c25f57cb9750
bragadeesh <bragadeesh@users.noreply.github.com>
2017-11-07 16:46:21 -0800

    fixing more issues, add rmse in ref check

--- library/src/include/ref_cpu.h
+++ library/src/include/ref_cpu.h
@@ -95,6 +95,7 @@ class RefLibOp
     local_fftwf_complex *in; // input
     local_fftwf_complex *ot; // output from fftw 
     local_fftwf_complex *lb; // output from lib
+    size_t totalSize;
     
     void DataSetup(const void *data_p)
     {
@@ -103,17 +104,17 @@ class RefLibOp
     
         DeviceCallIn *data = (DeviceCallIn *)data_p;
    
-        size_t totalSize = sizeof(local_fftwf_complex);
-        totalSize *= data->node->batch;
+        totalSize = data->node->batch;
         for(size_t i=0; i<data->node->length.size(); i++) totalSize *= data->node->length[i];
+        size_t totalByteSize = totalSize*sizeof(local_fftwf_complex);
 
-        in = (local_fftwf_complex *)local_fftwf_malloc(totalSize);
-        ot = (local_fftwf_complex *)local_fftwf_malloc(totalSize);
-        lb = (local_fftwf_complex *)local_fftwf_malloc(totalSize);
+        in = (local_fftwf_complex *)local_fftwf_malloc(totalByteSize);
+        ot = (local_fftwf_complex *)local_fftwf_malloc(totalByteSize);
+        lb = (local_fftwf_complex *)local_fftwf_malloc(totalByteSize);
         
-        memset(in, 0x40, totalSize);
-        memset(ot, 0x40, totalSize);
-        memset(lb, 0x40, totalSize);
+        memset(in, 0x40, totalByteSize);
+        memset(ot, 0x40, totalByteSize);
+        memset(lb, 0x40, totalByteSize);
     }
 
     void CopyVector(local_fftwf_complex *dst, local_fftwf_complex *src, size_t batch, size_t dist, std::vector<size_t> length, std::vector<size_t> stride)
@@ -184,29 +185,50 @@ nested_exit:
 
         if(data->node->scheme == CS_KERNEL_STOCKHAM)
         {
-        RefLibHandle &refHandle = RefLibHandle::GetRefLibHandle();
-        ftype_fftwf_plan_many_dft local_fftwf_plan_many_dft = (ftype_fftwf_plan_many_dft)dlsym(refHandle.fftw3f_lib, "fftwf_plan_many_dft");
-        ftype_fftwf_execute local_fftwf_execute = (ftype_fftwf_execute)dlsym(refHandle.fftw3f_lib, "fftwf_execute");
-        ftype_fftwf_destroy_plan local_fftwf_destroy_plan = (ftype_fftwf_destroy_plan)dlsym(refHandle.fftw3f_lib, "fftwf_destroy_plan");
-
-
-        int n[1];
-        n[0] = data->node->length[0];
-        int howmany = data->node->batch;
-        for(size_t i=1; i<data->node->length.size(); i++) howmany *= data->node->length[i];
-
-        void *p = local_fftwf_plan_many_dft(1, n, howmany, in, NULL, 1, n[0], ot, NULL, 1, n[0], LOCAL_FFTW_FORWARD, LOCAL_FFTW_ESTIMATE);
-        CopyInputVector(data_p);
-        local_fftwf_execute(p);
-        local_fftwf_destroy_plan(p);
+            RefLibHandle &refHandle = RefLibHandle::GetRefLibHandle();
+            ftype_fftwf_plan_many_dft local_fftwf_plan_many_dft = (ftype_fftwf_plan_many_dft)dlsym(refHandle.fftw3f_lib, "fftwf_plan_many_dft");
+            ftype_fftwf_execute local_fftwf_execute = (ftype_fftwf_execute)dlsym(refHandle.fftw3f_lib, "fftwf_execute");
+            ftype_fftwf_destroy_plan local_fftwf_destroy_plan = (ftype_fftwf_destroy_plan)dlsym(refHandle.fftw3f_lib, "fftwf_destroy_plan");
+    
+            int n[1];
+            n[0] = data->node->length[0];
+            int howmany = data->node->batch;
+            for(size_t i=1; i<data->node->length.size(); i++) howmany *= data->node->length[i];
+    
+            void *p = local_fftwf_plan_many_dft(1, n, howmany, in, NULL, 1, n[0], ot, NULL, 1, n[0], LOCAL_FFTW_FORWARD, LOCAL_FFTW_ESTIMATE);
+            CopyInputVector(data_p);
+            local_fftwf_execute(p);
+            local_fftwf_destroy_plan(p);
         }
         else if(data->node->scheme == CS_KERNEL_TRANSPOSE)
         {
+            CopyInputVector(data_p);
+
+            int howmany = data->node->batch;
+            for(size_t i=2; i<data->node->length.size(); i++) howmany *= data->node->length[i];
+
+            size_t cols = data->node->length[0];
+            size_t rows = data->node->length[1];
+            for(size_t b=0; b<howmany; b++)
+            {
+                for(size_t i=0; i<rows; i++)
+                {
+                    for(size_t j=0; j<cols; j++)
+                    {
+                        ot[b*rows*cols + j*rows + i][0] = in[b*rows*cols + i*cols + j][0];
+                        ot[b*rows*cols + j*rows + i][1] = in[b*rows*cols + i*cols + j][1];
+                    }
+                }
+            }
+        }
+        else
+        {
+            assert(false);
         }
     }
 
 public:
-    RefLibOp(const void *data_p) : in(nullptr), ot(nullptr)
+    RefLibOp(const void *data_p) : in(nullptr), ot(nullptr), lb(nullptr), totalSize(0)
     {
         DataSetup(data_p);
         Execute(data_p);
@@ -222,17 +244,51 @@ public:
         hipMemcpy(tmp_mem, data->bufOut[0], out_size_bytes, hipMemcpyDeviceToHost);
         CopyVector(lb, tmp_mem, data->node->batch, data->node->oDist, data->node->length, data->node->outStride);
 
+        double maxMag = 0.0;
+        double rmse = 0.0, nrmse = 0.0;
+        for(size_t i=0; i<totalSize; i++)
+        {
+            double mag;
+            double ex_r, ex_i, ac_r, ac_i;
+
+            ac_r = lb[i][0];
+            ac_i = lb[i][1];
+            ex_r = ot[i][0];
+            ex_i = ot[i][1];
+
+            mag = ex_r*ex_r + ex_i*ex_i;
+            maxMag = (mag > maxMag) ? mag : maxMag;
+
+            rmse += ((ex_r - ac_r)*(ex_r - ac_r) + (ex_i - ac_i)*(ex_i - ac_i));
+        }
+
+        maxMag = sqrt(maxMag);
+        rmse = sqrt(rmse/(double)totalSize);
+        nrmse = rmse/maxMag;
+
+        std::cout << "---------------------------------------------" << std::endl;
+        std::cout << "rmse: " << rmse << std::endl << "nrmse: " << nrmse << std::endl;
+        std::cout << "---------------------------------------------" << std::endl;
+         
+#if 0
+        std::cout << "input" << std::endl;
+        for(size_t i=0; i<4; i++)
+        {
+            std::cout << in[i][0] << ", " << in[i][1] << std::endl;
+        }
+
         std::cout << "lib output" << std::endl;
-        for(size_t i=0; i<16; i++)
+        for(size_t i=0; i<4; i++)
         {
             std::cout << lb[i][0] << ", " << lb[i][1] << std::endl;
         }
 
         std::cout << "fftw output" << std::endl;
-        for(size_t i=0; i<16; i++)
+        for(size_t i=0; i<4; i++)
         {
             std::cout << ot[i][0] << ", " << ot[i][1] << std::endl;
         }
+#endif
 
         if(tmp_mem)
             free(tmp_mem);
